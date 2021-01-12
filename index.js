12f3ea4839a75802c6cb921925db771e6fefd70f
NEIN <8bu2010@gmail.com>
2021-01-12 17:49:51 +0700

    update code

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const { chromium } = require("playwright");
 (async () => {
-  const browser = await chromium.launch({ headless: false });
-  const context = await browser.newContext();
+  const browser = await chromium.launch({ headless: true });
+  const context = await browser.newContext({});
   const page = await context.newPage();
 
+  const triggerCountDown = (duration) => {
+    setTimeout(() => {
+      // process.stdout.clearLine();  // clear current text
+      // process.stdout.cursorTo(0);  // move cursor to beginning of line
+      process.stdout.write(`${duration/1000} second(s) left!\r`);
+      if (duration > 1000) {
+        triggerCountDown(duration - 1000);
+      }
+    }, 1000);
+  }
+  
+
   await page.goto("https://testnet.binance.org/faucet-smart");
-
-  await page.setViewportSize({ width: 1536, height: 430 });
-
+  console.debug('GOTO: https://testnet.binance.org/faucet-smart')
   await page.waitForSelector(".container #url");
   await page.fill(
     ".container #url",
     "0x160A41F6D39C4411a4b6bC4A54FA2DD6a7763094"
   );
+  console.debug('INPUT: 0x160A41F6D39C4411a4b6bC4A54FA2DD6a7763094')
+
   const getBNB = async () => {
     await page.waitForSelector(
       ".col-lg-8 > .input-group > .input-group-btn:nth-child(2) > .btn > .fa"
@@ -27,7 +39,8 @@ const { chromium } = require("playwright");
     await page.click(
       ".input-group > .open > .dropdown-menu > li:nth-child(3) > a"
     );
-    console.debug('CLICKED GET BNB');
+    console.debug('REQUESTED 6.2 BNB');
+    triggerCountDown(480000);
   };
   await getBNB();
 
