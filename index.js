19c0f86d99c2137c070ed173558def7961c8151f
NEIN <8bu2010@gmail.com>
2021-01-12 16:42:42 +0700

    headless false

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { chromium } = require("playwright");
 (async () => {
-  const browser = await chromium.launch();
+  const browser = await chromium.launch({ headless: false });
   const context = await browser.newContext();
   const page = await context.newPage();
 
